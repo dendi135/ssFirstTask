938a7f20d11bea304c0dc886cdc7f87cc590a8e8
dnedi135 <themakc135@gmail.com>
2019-06-19 11:18:53 +0300

    Created dynamic DataProvider

--- Task/src/test/java/com/softserve/firsttask/tests/ValuesTest.java
+++ Task/src/test/java/com/softserve/firsttask/tests/ValuesTest.java
@@ -1,35 +1,34 @@
 package com.softserve.firsttask.tests;
 
+import com.softserve.firsttask.data.City;
 import com.softserve.firsttask.data.PlacesRepository;
 import com.softserve.firsttask.pages.SearchPlaceComponent;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class ValuesTest extends TestRunner {
 
     @DataProvider(name = "valuesProvider")
-    public Object[][] correctData() {
-        return new Object[][]{
-                {PlacesRepository.fromExcel().get(0).getName(),
-                        PlacesRepository.fromExcel().get(0).getLatitude(),
-                        PlacesRepository.fromExcel().get(0).getLongtitude()},
-                {PlacesRepository.fromExcel().get(1).getName(),
-                        PlacesRepository.fromExcel().get(1).getLatitude(),
-                        PlacesRepository.fromExcel().get(1).getLongtitude()},
-                {PlacesRepository.fromExcel().get(2).getName(),
-                        PlacesRepository.fromExcel().get(2).getLatitude(),
-                        PlacesRepository.fromExcel().get(2).getLongtitude()}
-        };
+    public Iterator<Object[]> correctData() {
+        Set<Object[]> list = new LinkedHashSet<>();
+        for(City city : PlacesRepository.fromExcel()) {
+            list.add((new Object[] {city.getName(), city.getLatitude(), city.getLongtitude()}));
+        }
+        return list.iterator();
     }
 
     @Test(dataProvider = "valuesProvider")
-    public void checkValues(String expectedName, String expectedLat, String expectedLong) {
+    public void checkValues(Object[] cityData) {
         SearchPlaceComponent searchPlaceComponent = loadApplication()
-                .findByPlaceName(expectedName)
+                .findByPlaceName(cityData[0].toString())
                 .getGrayBoxComponent();
-        Assert.assertEquals(searchPlaceComponent.getLatitudeValue(), expectedLat);
-        Assert.assertEquals(searchPlaceComponent.getLongitudeValue(), expectedLong);
+        Assert.assertEquals(searchPlaceComponent.getLatitudeValue(), cityData[1].toString());
+        Assert.assertEquals(searchPlaceComponent.getLongitudeValue(), cityData[2].toString());
     }
 
 }
