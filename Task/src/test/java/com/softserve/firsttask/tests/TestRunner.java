1fbdb94a79e76f0fefc0b87d4f91a570647c70a4
dnedi135 <themakc135@gmail.com>
2019-06-19 16:31:30 +0300

    Added test running with remote driver

--- Task/src/test/java/com/softserve/firsttask/tests/TestRunner.java
+++ Task/src/test/java/com/softserve/firsttask/tests/TestRunner.java
@@ -1,11 +1,13 @@
 package com.softserve.firsttask.tests;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import com.softserve.firsttask.pages.HomePage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -19,10 +21,11 @@ public abstract class TestRunner {
     private final String SERVER_URL = "https://www.latlong.net/";
 
     @BeforeClass
-    public void beforeClass() {
-        URL url = this.getClass().getResource("/chromedriver-osx-32bit");
-        System.setProperty("webdriver.chrome.driver", url.getPath());
-        driver = new ChromeDriver();
+    public void beforeClass() throws MalformedURLException {
+        driver = new RemoteWebDriver(
+                new URL("http://localhost:4444/wd/hub"),
+                DesiredCapabilities.chrome()
+        );
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
